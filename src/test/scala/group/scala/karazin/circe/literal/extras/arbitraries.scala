7aff1ae625d40eb6c81e61aa278952c8004c68d8
vladislav070 <shramenko.vlad@gmail.com>
2021-05-12 18:18:40 +0300

    CLE-LDS-SGA - Added arbitrary for NonEmptyVector and NonEmptySet.

--- src/test/scala/group/scala/karazin/circe/literal/extras/arbitraries.scala
+++ src/test/scala/group/scala/karazin/circe/literal/extras/arbitraries.scala
@@ -1,10 +1,13 @@
 package group.scala.karazin.circe.literal.extras
 
 import cats.implicits._
-import cats.data.{NonEmptyList}
+import cats.data.{NonEmptyList, NonEmptySet, NonEmptyVector}
+import cats.kernel.Order
 import org.scalacheck._
 import org.scalacheck.Prop._
 import org.scalacheck.util._
+
+import scala.collection.immutable.SortedSet
 
 object arbitraries {
   def genNonEmptyList[T: Arbitrary]: Gen[NonEmptyList[T]] = for {
@@ -12,5 +15,17 @@ object arbitraries {
     tail <- Arbitrary.arbitrary[List[T]]
   } yield NonEmptyList(head, tail)
 
+  def genNonEmptyVector[T: Arbitrary]: Gen[NonEmptyVector[T]] = for {
+    head <- Arbitrary.arbitrary[T]
+    tail <- Arbitrary.arbitrary[Vector[T]]
+  } yield NonEmptyVector(head, tail)
+
+  def genNonEmptySet[T: Arbitrary](using ord: Ordering[T], order: Order[T]): Gen[NonEmptySet[T]] = for {
+    head <- Arbitrary.arbitrary[T]
+    set <- Arbitrary.arbitrary[Set[T]]
+  } yield NonEmptySet(head, SortedSet[T]() ++ set)
+
   given [T: Arbitrary]: Arbitrary[NonEmptyList[T]] = Arbitrary(genNonEmptyList[T])
+  given [T: Arbitrary]: Arbitrary[NonEmptyVector[T]] = Arbitrary(genNonEmptyVector[T])
+  given [T: Arbitrary](using ord: Ordering[T], order: Order[T]): Arbitrary[NonEmptySet[T]] = Arbitrary(genNonEmptySet[T])
 }
