5bc52c597c0b2183497550a212322ecfe2a8c7a0
Mykhail Zakharevych <mykhail.zakharevych@chisw.com>
2021-05-12 15:34:18 +0300

    * added Json Object generator

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  
+
   object Version {
     val circe = "0.14.0-M6"
     val munit = "0.7.25"
@@ -13,7 +13,8 @@ object Dependencies {
     "io.circe" %% "circe-parser",
     "io.circe" %% "circe-numbers",
     "io.circe" %% "circe-jawn",
-    "io.circe" %% "circe-pointer"
+    "io.circe" %% "circe-pointer",
+    "io.circe" %% "circe-testing"
   ).map(_ % Version.circe withSources() withJavadoc())
 
   lazy val munit: Seq[ModuleID] = Seq(
---
+++ src/test/scala/group/scala/karazin/circe/literal/extras/generators/JsonGenerators.scala
@@ -0,0 +1,22 @@
+package group.scala.karazin.circe.literal.extras.generators
+
+import cats.implicits._
+import group.scala.karazin.circe.literal.extras.generators.JsonGenerators.genJson
+import io.circe.syntax._
+import io.circe.testing.instances.arbitraryJsonObject
+import io.circe.{Json, JsonNumber, JsonObject, parser}
+import org.scalacheck.Gen
+import org.scalacheck.Arbitrary
+import org.scalacheck.Prop._
+import io.circe.Encoder.encodeJsonObject
+
+/**
+  * as an option, we can put all our json generation methods (incl. custom ones if need be)
+  * in a separate file, like here
+  */
+object JsonGenerators {
+
+  val genJsonObjectDefault: Gen[JsonObject] =
+    arbitraryJsonObject.arbitrary.sample.get
+
+}
