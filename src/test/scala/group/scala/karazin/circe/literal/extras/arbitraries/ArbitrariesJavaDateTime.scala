0d5d19a0ac4ae62b0602c0d3e990753c4c008c02
Mariia Shvedchenko <mashylka81@gmail.com>
2021-05-18 16:30:23 +0300

    CLE-NST-AAJ - Add Arbitraries for Java date time - possible code is reused

--- src/test/scala/group/scala/karazin/circe/literal/extras/arbitraries/ArbitrariesJavaDateTime.scala
+++ src/test/scala/group/scala/karazin/circe/literal/extras/arbitraries/ArbitrariesJavaDateTime.scala
@@ -23,7 +23,6 @@ trait ArbitrariesJavaDateTime {
     for {
       zone <- Gen.oneOf((ZoneId.getAvailableZoneIds).asScala)
     } yield ZoneId.of(zone)
-
   }
 
   val localDateGenerator: Gen[LocalDate] = Gen.choose(LocalDate.MIN, LocalDate.MAX)
@@ -57,8 +56,8 @@ trait ArbitrariesJavaDateTime {
     for {
       localDate <- Gen.choose(LocalDate.MIN, LocalDate.MAX)
       localTime <- Gen.choose(LocalTime.MIN, LocalTime.MAX)
-      zone      <- Gen.oneOf((ZoneId.getAvailableZoneIds).asScala)
-    } yield ZonedDateTime.of(localDate, localTime, ZoneId.of(zone))
+      zone      <- zoneIdGenerator
+    } yield ZonedDateTime.of(localDate, localTime, zone)
 
   val zoneOffsetGenerator: Gen[ZoneOffset] = Gen.choose(ZoneOffset.MIN, ZoneOffset.MAX)
 
