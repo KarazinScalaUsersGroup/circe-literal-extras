c4feb7798307cb8eb8f4332b6c9f3d3a98b7e660
michael.zakharevych <somestrangerua@gmail.com>
2021-05-19 14:32:21 +0300

    * added tests for Map

---
+++ src/test/scala/group/scala/karazin/circe/literal/extras/MapEncodeSuite.scala
@@ -0,0 +1,129 @@
+package group.scala.karazin.circe.literal.extras
+
+import io.circe._
+import io.circe.syntax._
+import org.scalacheck._
+import org.scalacheck.Prop._
+import group.scala.karazin.circe.literal.extras.arbitraries.instances.{given, _}
+
+class MapEncodeSuite extends munit.ScalaCheckSuite:
+
+  property("inlined Map with Int values") {
+
+    extension (inline sc: StringContext)
+      inline def encode(inline args: Any*): Json =
+        ${ macros.encode[Map[String, Int]]('sc, 'args) }
+
+    forAll { (map: Map[String, Int]) =>
+
+      val result = encode"$map"
+
+      val expected = map.asJson
+
+      assertEquals(result, expected)
+
+    }
+  }
+
+  property("inlined Map of Option Int values") {
+
+    extension (inline sc: StringContext)
+      inline def encode(inline args: Any*): Json =
+        ${macros.encode[Map[String, Option[Int]]]('sc, 'args)}
+
+    forAll { (map: Map[String, Option[Int]]) =>
+
+      val result: Json = encode"$map"
+
+      val expected: Json = map.asJson
+
+      assertEquals(result, expected)
+
+    }
+
+  }
+
+  property("inlined Map of primitives with Int values") {
+
+    case class Primitive(value: Int) derives Codec.AsObject
+
+    given Arbitrary[Primitive] = Arbitrary(Arbitrary.arbitrary[Int].map(Primitive(_)))
+
+    extension (inline sc: StringContext)
+      inline def encode(inline args: Any*): Json =
+        ${macros.encode[Map[String, Primitive]]('sc, 'args)}
+
+    forAll { (map: Map[String, Primitive]) =>
+
+      val result: Json = encode"$map"
+
+      val expected: Json = map.asJson
+
+      assertEquals(result, expected)
+
+    }
+
+  }
+
+
+  property("inlined Map of Option primitives") {
+
+    case class Primitive(value: Int) derives Codec.AsObject
+
+    given Arbitrary[Primitive] = Arbitrary(Arbitrary.arbitrary[Int].map(Primitive(_)))
+
+    extension (inline sc: StringContext)
+      inline def encode(inline args: Any*): Json =
+        ${macros.encode[Map[String, Option[Primitive]]]('sc, 'args)}
+
+    forAll { (map: Map[String, Option[Primitive]]) =>
+
+      val result: Json = encode"$map"
+
+      val expected: Json = map.asJson
+
+      assertEquals(result, expected)
+
+    }
+
+  }
+
+  property("inlined Map of JsonObject values") {
+
+    extension (inline sc: StringContext)
+      inline def encode(inline args: Any*): Json =
+        ${macros.encode[Map[String, JsonObject]]('sc, 'args)}
+
+    forAll { (map: Map[String, JsonObject]) =>
+
+      val result: Json = encode"$map"
+
+      val expected: Json = map.asJson
+
+      assertEquals(result, expected)
+
+    }
+
+  }
+
+  property("inlined Map of primitives with JsonObject values") {
+
+    case class Primitive(value: JsonObject) derives Codec.AsObject
+
+    given Arbitrary[Primitive] = Arbitrary(Arbitrary.arbitrary[JsonObject].map(Primitive(_)))
+
+    extension (inline sc: StringContext)
+      inline def encode(inline args: Any*): Json =
+        ${macros.encode[Map[String, Primitive]]('sc, 'args)}
+
+    forAll { (map: Map[String, Primitive]) =>
+
+      val result: Json = encode"$map"
+
+      val expected: Json = map.asJson
+
+      assertEquals(result, expected)
+
+    }
+
+  }
