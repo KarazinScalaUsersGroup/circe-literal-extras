c89bf0b55312c38ad2dd8652181bab700b1e724c
michael.zakharevych <somestrangerua@gmail.com>
2021-06-15 18:27:09 +0300

    * added compile error tests

--- src/test/scala/group/scala/karazin/circe/literal/extras/suites/SetEncodeSuite.scala
+++ src/test/scala/group/scala/karazin/circe/literal/extras/suites/SetEncodeSuite.scala
@@ -129,3 +129,30 @@ class SetEncodeSuite extends munit.ScalaCheckSuite:
     }
 
   }
+
+  test("corrupted ints parsing compile error") {
+    scala.compiletime.testing.typeCheckErrors(
+      """
+          extension (inline sc: StringContext)
+            inline def encode(inline args: Any*): Json =
+              ${ macros.encode[Set[Int]]('sc, 'args) }
+
+          encode"[ -1, null ]"
+        """
+    ).headOption match
+      case Some(error) => assert(error.message.startsWith("Encode error:"))
+      case _           => fail("No compilation error was found.")
+  }
+
+  test("corrupted options parsing compile error") {
+    scala.compiletime.testing.typeCheckErrors(
+      """
+          extension (inline sc: StringContext)
+            inline def encode(inline args: Any*): Json =
+              ${ macros.encode[Set[Option[Int]]]('sc, 'args) }
+          encode"[ null, 42, { } ]"
+      """
+    ).headOption match
+      case Some(error) => assert(error.message.startsWith("Encode error:"))
+      case _           => fail("No compilation error was found.")
+  }
